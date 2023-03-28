6881bbc0b9ae65df79d0c955c2e306ce813261e6
Kevin Laminto <kevin.laminto@gmail.com>
2023-03-28 15:52:48 +1100

    Initial Commit

---
+++ Weather/WeatherApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherApp.swift
+//  Weather
+//
+//  Created by Kevin Laminto on 28/3/2023.
+//
+
+import SwiftUI
+
+@main
+struct WeatherApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
